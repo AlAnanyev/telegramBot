db239f68f0a881dfb35317841784a9a244fe1e4f
alexey156 <78699841+alexey156@users.noreply.github.com>
2021-02-07 23:46:37 +0300

    addet 1parser

--- main.py
+++ main.py
@@ -2,43 +2,56 @@ import telebot
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.labirint.ru/gift/'
+URL = 'https://1xstavka.ru/live/Table-Tennis/2064427-Masters/'
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36',
            'accept': '*/*'}
-HOST = 'https://www.labirint.ru'
+HOST = 'https://1xstavka.ru/'
 
-lowvalue = 0
-highvalue = 0
 
 def get_html(url, params = None):
     r = requests.get(url, headers=HEADERS, params=params)
     return r
 
 
-def get_content(html, high_border, low_border):
+def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
-    items = soup.find_all('div', class_='product-padding')
-    books = []
+    items = soup.find_all('div', class_='c-events__item c-events__item_col')
+    # print(items)
+    matches = []
     for item in items:
-        price = item.find('span', class_="price-gray").get_text()
-        price = price.replace(" ", "")
-        if high_border > int(price) > low_border:
-            books.append(
-            {
-                'Title': item.find('span', class_="product-title").get_text(),
-                'Link': HOST + item.find('a', class_='cover').get('href')
-            }
-            )
-    return books
+        # счёт по сетам
+        set = item.find_all('span', class_='c-events-scoreboard__cell c-events-scoreboard__cell--all')
+        if len(set) == 0:
+            set_text1 = '0'
+            set_text2 = '0'
+        else:
+            set_text1 = set[0].get_text()
+            set_text2 = set[1].get_text()
+        # линии со счётом в сетах
+        schetlines = item.find_all('div', class_='c-events-scoreboard__line')
+        # коэффициенты
+        bets = item.find('div', class_='c-bets')
+        matches.append(
+        {
+            'Title': item.find('span', class_="n").get_text(),
+            'Link': HOST + item.find('a', class_='c-events__name').get('href'),
+            'set': set_text1 + '--' + set_text2,
+            'schetline1': schetlines[0].get_text().replace('\n', ' '),
+            'schetline2': schetlines[1].get_text().replace('\n', ' '),
+            'bets': bets.get_text().replace('\n', ' ')
+
+        }
+        )
+    return matches
 
 
-def parse(high_border, low_border):
+def parse():
     html = get_html(URL)
     if html.status_code == 200:
-        books = get_content(html.text, high_border, low_border)
+        matches = get_content(html.text)
     else:
         return 'Error'
-    return books
+    return matches
 
 
 bot = telebot.TeleBot('1699645072:AAGE3eWMl-spPf7vCJNphWQFQMUlz6k6D4A')
@@ -65,21 +78,21 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    global lowvalue
-    global highvalue
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, 'Привет, мой создатель')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'лабиринт':
-        books = parse(int(highvalue), int(lowvalue))
-        for book in books:
-            bot.send_message(message.chat.id, book.get('Title'))
+    elif message.text.lower() == 'матчи':
+        matches = parse()
+        for match in matches:
+            bot.send_message(message.chat.id, 'Мастерс' + match.get('Title'))
+            bot.send_message(message.chat.id, match.get('Link'))
+            bot.send_message(message.chat.id, 'счёт по сетам ' + match.get('set'))
+            bot.send_message(message.chat.id, 'счёт в сетах 1-го' + match.get('schetline1'))
+            bot.send_message(message.chat.id, 'счёт в сетах 2-го' + match.get('schetline2'))
+            bot.send_message(message.chat.id, 'какие-то коэффициенты' + match.get('bets'))
 
-    elif message.text.find('Low__') == 0:
-        lowvalue = int(message.text.replace('Low__', ''))
-    elif message.text.find('High__') == 0:
-        highvalue = int(message.text.replace('High__', ''))
+
 
 
 bot.polling()
