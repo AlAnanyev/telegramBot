790d490c28c488029618f876c7471be3758dabcb
alexey156 <78699841+alexey156@users.noreply.github.com>
2021-02-09 23:46:23 +0300

    добавлены команды управления в боте

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ URL = ['https://1xstavka.ru/live/Table-Tennis/2178512-Winners-League/',
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36',
            'accept': '*/*'}
 HOST = 'https://1xstavka.ru/'
-POROG = 5  # пороговый счет в партии
+POROGWINSET = 5  # пороговый счет в партии
 POROGKOEF = 1.4  # пороговый коэффициент
-CHECKEND = 0  # признак выхода из цикла запросов
+ENDCOMMAND = 0  # команда завершения работы парсера
+SIGNAL = -1 # команда на вывод всех сигналов
 
-def get_html(url, params = None):
+def get_html(url, params=None):  # реквест, получение ответа
     r = requests.get(url, headers=HEADERS, params=params)
     return r
 
@@ -49,8 +50,6 @@ def get_content(html):
             set2 = '0'
         else:
             set2 = schetline2.pop(0)  # первый элемент - победы в сетах, удаляем и присваиваем в set
-
-
         # коэффициенты
         bets = item.find('div', class_='c-bets').get_text().split('\n')
         bet1 = bets[2]
@@ -72,17 +71,17 @@ def get_content(html):
 
 def proverka_na_pobedu(match):
 
-    if match.get('bet1') == '-' or match.get('bet2') == '-':
+    if match.get('bet1') == '-' or match.get('bet2') == '-': # условие для коэфиц. без значений
         return -1
 
-    if float(match.get('bet1')) >= POROGKOEF:
-        if int(match.get('set1')) == 1 and int(match.get('set2')) == 0:
-            if int(match.get('schetline2')[0]) <= POROG:
+    if float(match.get('bet1')) >= POROGKOEF:  # сравение с порогом первого коэф
+        if int(match.get('set1')) == 1 and int(match.get('set2')) == 0:   # проверка на одну победу в партии
+            if int(match.get('schetline2')[0]) <= POROGWINSET:
                 res1 = 'sledim'  # на слежение
             else:
                 res1 = 'pas'
-        elif int(match.get('set1')) == 2 and int(match.get('set2')) == 0:
-            if int(match.get('schetline2')[0]) <= POROG and int(match.get('schetline2')[1]) <= POROG:
+        elif int(match.get('set1')) == 2 and int(match.get('set2')) == 0:  # проверка на две победы в партии
+            if int(match.get('schetline2')[0]) <= POROGWINSET and int(match.get('schetline2')[1]) <= POROGWINSET:
                 res1 = 'stavka'  # на ставку
             else:
                 res1 = 'pas'
@@ -91,14 +90,14 @@ def proverka_na_pobedu(match):
     else:
         res1 = 'pas'
 
-    if float(match.get('bet2')) >= POROGKOEF:
-        if int(match.get('set2')) == 1 and int(match.get('set1')) == 0:
-            if int(match.get('schetline1')[0]) <= POROG:
+    if float(match.get('bet2')) >= POROGKOEF:   # сравение с порогом второго коэф
+        if int(match.get('set2')) == 1 and int(match.get('set1')) == 0:  # проверка на одну победу в партии
+            if int(match.get('schetline1')[0]) <= POROGWINSET:
                 res2 = 'sledim'  # на слежение
             else:
                 res2 = 'pas'
-        elif int(match.get('set2')) == 2 and int(match.get('set1')) == 0:
-            if int(match.get('schetline1')[0]) <= POROG and int(match.get('schetline1')[1]) <= POROG:
+        elif int(match.get('set2')) == 2 and int(match.get('set1')) == 0:  # проверка на две победы в партии
+            if int(match.get('schetline1')[0]) <= POROGWINSET and int(match.get('schetline1')[1]) <= POROGWINSET:
                 res2 = 'stavka'  # на ставку
             else:
                 res2 = 'pas'
@@ -107,6 +106,7 @@ def proverka_na_pobedu(match):
     else:
         res2 = 'pas'
 
+    # возврат результатов
     if res1 == 'stavka':
         return 1
     elif res2 == 'stavka':
@@ -120,53 +120,124 @@ def proverka_na_pobedu(match):
 
 
 def parse():
-    matches = []
+    matches = []  # список матчей
     for url in URL:
         html = get_html(url)
         if html.status_code == 200:
             mat = get_content(html.text)
             matches += mat
-    print('запрос')
     return matches
 
 
 def loop_zapros():
-    while 1:
-        bot.send_message(362390015, 'проверка')
-
-        time.sleep(4)
+    global ENDCOMMAND
+    while ENDCOMMAND != 1:
+        print('запрос')
+        matches = parse()
+        for match in matches:
+            result_proverki = proverka_na_pobedu(match)
+            if SIGNAL == 1:
+                if result_proverki == 1 or result_proverki == 2:
+                    bot.send_message(362390015,
+                                 match.get('Title') + '\n' +
+                                 match.get('Link') + '\n' +
+                                 'Cчёт по сетам: ' + match.get('set1') + '--' + match.get('set2') + '\n' +
+                                 'Счёт в сетах 1-го: ' + ''.join(
+                                     str(e + '\t') for e in match.get('schetline1')) + '\n' +
+                                 'Счёт в сетах 2-го: ' + ''.join(
+                                     str(e + '\t') for e in match.get('schetline2')) + '\n' +
+                                 'П1 = ' + match.get('bet1') + '\n' +
+                                 'П2 = ' + match.get('bet2')
+                                 )
+            elif SIGNAL == -1:
+                if result_proverki != -1:
+                    bot.send_message(362390015,
+                                 match.get('Title') + '\n' +
+                                 match.get('Link') + '\n' +
+                                 'Cчёт по сетам: ' + match.get('set1') + '--' + match.get('set2') + '\n' +
+                                 'Счёт в сетах 1-го: ' + ''.join(
+                                     str(e + '\t') for e in match.get('schetline1')) + '\n' +
+                                 'Счёт в сетах 2-го: ' + ''.join(
+                                     str(e + '\t') for e in match.get('schetline2')) + '\n' +
+                                 'П1 = ' + match.get('bet1') + '\n' +
+                                 'П2 = ' + match.get('bet2')
+                                 )
+        # bot.send_message(362390015, 'проверка')
+        print('конец запроса')
+        time.sleep(10)
+    ENDCOMMAND = 0
 
 
 bot = telebot.TeleBot('1699645072:AAGE3eWMl-spPf7vCJNphWQFQMUlz6k6D4A')
 
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=['start_parcer'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Старт работы парсера')
+    loop_zapros()
 
-@bot.message_handler(commands=['Value'])
+
+@bot.message_handler(commands=['stop_parcer'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, введи Low__цена, High__цена')
+    global ENDCOMMAND
+    ENDCOMMAND = 1
+    bot.send_message(message.chat.id, 'Парсер остановлен')
 
 
-# @bot.message_handler(content_types=['text'])
-# def send_text_values(message):
-#     global lowvalue
-#     global highvalue
-#     if message.text.find('Low__') == 0:
-#         lowvalue = int(message.text.replace('Low__', ''))
-#     elif message.text.find('High__') == 0:
-#         highvalue = int(message.text.replace('High__', ''))
+@bot.message_handler(commands=['vse'])
+def start_message(message):
+    global SIGNAL
+    SIGNAL = -1
+    bot.send_message(message.chat.id, 'выводятся все матчи')
+
+
+@bot.message_handler(commands=['stavka'])
+def start_message(message):
+    global SIGNAL
+    SIGNAL = 1
+    bot.send_message(message.chat.id, 'выводятся только матчи на ставку')
+
+
+@bot.message_handler(commands=['коэффициент'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Сейчас порог по коэффициенту = ' + str(POROGKOEF) + '. Чтобы поменять введи koef_<значение>')
+
+
+@bot.message_handler(commands=['партия'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Сейчас порог по счёту в партии = ' + str(POROGWINSET) + '. Чтобы поменять введи winset_<значение>')
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    print(message.text)
+    global POROGKOEF
+    global POROGWINSET
+    print(message.text, message.chat.id)
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, 'Привет, Поросенок')
-    elif message.text.lower() == 'старт':
-        loop_zapros()
-    elif message.text.lower() == 'test':
+
+    elif message.text.find('koef_') == 0: # смена коэффициента
+        message_size = len(message.text)
+        if message_size > 5:
+            sl = slice(5, message_size, 1)
+            new_koef = message.text[sl]
+            if new_koef.replace('.', '').isdigit():
+                POROGKOEF = float(new_koef)
+                bot.send_message(message.chat.id, 'Порог коэффициента >=' + str(POROGKOEF))
+                print(POROGKOEF)
+
+    elif message.text.find('winset_') == 0: # смена порога по выигрышам в партии
+        message_size = len(message.text)
+        if message_size > 7:
+            sl = slice(7, message_size, 1)
+            new_koef = message.text[sl]
+            if new_koef.isdigit():
+                POROGWINSET = int(new_koef)
+                bot.send_message(message.chat.id, 'Порог выигрышей в партии <=' + str(POROGWINSET))
+                print(POROGWINSET)
+
+    elif message.text.lower() == 'test': # тестовая ветка
         matches = parse()
         for match in matches:
             bot.send_message(message.chat.id,
@@ -178,8 +249,6 @@ def send_text(message):
                              'П1 = ' + match.get('bet1') + '\n' +
                              'П2 = ' + match.get('bet2')
                              )
-
-
 
 bot.polling()
 
